fecb12f9aa3a5e14f90b6f128f9d2ebcf7544d45
Chris Houghton and Kassidy Douglas <kcmdouglas@gmail.com>
2016-02-02 11:03:30 -0800

    Test getId methods for Status class and TypeTask class.

--- src/main/java/TypeTask.java
+++ src/main/java/TypeTask.java
@@ -12,12 +12,12 @@ public class TypeTask {
     }
   }
 
-  public static String getTypeTaskName(Int typeTaskId) {
-    String sql = "SELECT name FROM type_task WHERE id = :typeTaskId";
-    try (Connection con = DB.sql2o.open()) {
-      return (int) con.createQuery(sql)
-      .addParameter("typeTaskId", typeTaskId)
-      .executeScalar(String.class);
-    }
-  }
+  // public static String getTypeTaskName(int typeTaskId) {
+  //   String sql = "SELECT name FROM type_task WHERE id = :typeTaskId";
+  //   try (Connection con = DB.sql2o.open()) {
+  //     return con.createQuery(sql)
+  //     .addParameter("typeTaskId", typeTaskId)
+  //     .executeAndFetch(String.class);
+  //   }
+  // }
 }
