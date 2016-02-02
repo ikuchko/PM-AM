0a33bfa9009fd5ef806367e5ec004eca48a82816
Chris Houghton and Kassidy Douglas <choughton71@gmail.com>
2016-02-02 09:59:51 -0800

    Add Status and TypeTask classes.

---
+++ src/main/java/TypeTask.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
+
+public class TypeTask {
+  public static int getId(String type) {
+    String sql = "SELECT id FROM type_task WHERE name LIKE :input";
+    try (Connection con = DB.sql2o.open()) {
+      return (int) con.createQuery(sql)
+      .addParameter("input", type)
+      .executeScalar(Integer.class);
+    }
+  }
+
+  public static String getTypeTaskName(Int typeTaskId) {
+    String sql = "SELECT name FROM type_task WHERE id = :typeTaskId";
+    try (Connection con = DB.sql2o.open()) {
+      return (int) con.createQuery(sql)
+      .addParameter("typeTaskId", typeTaskId)
+      .executeScalar(String.class);
+    }
+  }
+}
