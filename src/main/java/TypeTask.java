42944cd1cdfb042cba90067bce947ec5a1cb47a1
Illia Kuchko and Nathan Romike <student@epicodus.com>
2016-02-02 12:12:52 -0800

    Add removed files Status and TypeTask.

---
+++ src/main/java/TypeTask.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
+
+public class TypeTask {
+  public static int getId(String type) {
+    String sql = "SELECT id FROM type_task WHERE name LIKE :input";
+    try (Connection con = DB.sql2o.open()) {
+      return (int) con.createQuery(sql)
+      .addParameter("input", type)
+      .executeScalar(Integer.class);
+    }
+  }
+
+  // public static String getTypeTaskName(int typeTaskId) {
+  //   String sql = "SELECT name FROM type_task WHERE id = :typeTaskId";
+  //   try (Connection con = DB.sql2o.open()) {
+  //     return con.createQuery(sql)
+  //     .addParameter("typeTaskId", typeTaskId)
+  //     .executeAndFetch(String.class);
+  //   }
+  // }
+}
+Status API Training Shop Blog About Pricing
