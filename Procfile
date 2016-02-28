3c37acbb4dc7efc32923783514b4e4c4bffa85b2
Illia <ikuchko@gmail.com>
2016-02-28 15:37:51 -0800

    configure project for deployment to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/PMAM/bin/PMAM
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,15 @@ import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
+    ProcessBuilder process = new ProcessBuilder();
+     Integer port;
+     if (process.environment().get("PORT") != null) {
+         port = Integer.parseInt(process.environment().get("PORT"));
+     } else {
+         port = 4567;
+     }
+
+    setPort(port);
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
